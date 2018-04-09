ba22c15c23654f303f8f34ee9250365f28a6f4a7
Matt Helmus <mrhelmus@gmail.com>
2018-04-09 15:59:31 -0400

    edits

--- natdb_install.R
+++ natdb_install.R
@@ -4,10 +4,18 @@ install.packages("devtools")
 # then load devtools
 require(devtools)
 
+#then install curl
+install.packages("curl")
+
+#then install suppdata
+install_github("willpearse/suppdata")
+
 # then you need to install the testdat package
 install_github("ropensci/testdat")
 
 # then you install the package natdb
 install_github("willpearse/natdb")
 
+#Note if you still have a failure R will give you a warning of the package that you need to install, so install that package that R tells you to install and then again try to install natdb 
+install_github("willpearse/natdb")
 
